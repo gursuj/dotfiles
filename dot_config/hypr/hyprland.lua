bebef10fb15ae7b8d74efdccf8f650f728d99dd8
gursuj <86787475+gursuj@users.noreply.github.com>
2026-08-29 15:17:30 +0545

    arch: make laptop be primary monitor

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -76,8 +76,8 @@ end)
 -- hl.monitor({ output = "eDP-1", mode = "1366x768@60", position = "0x0", scale = 1 })
 
 -- dual monitor home setup
-hl.monitor({ output = "HDMI-A-1", mode = "preferred",     position = "0x0",    scale = 1 })
-hl.monitor({ output = "eDP-1",    mode = "1366x768@60",   position = "1366x0", scale = 1 })
+hl.monitor({ output = "HDMI-A-1", mode = "preferred",     position = "1366x0",    scale = 1 })
+hl.monitor({ output = "eDP-1",    mode = "1366x768@60",   position = "0x0", scale = 1 })
 
 -- screen mirroring setup
 -- hl.monitor({ output = "eDP-1",    mode = "preferred", position = "0x0",  scale = 1 })
